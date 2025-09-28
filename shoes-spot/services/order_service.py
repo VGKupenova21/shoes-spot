cd5936cd3f794f8f13259ba2de2c6a5f0474e80d
Viktoria <VGKupenova21@codingburgas.bg>
2025-09-28 21:16:40 +0300

    Update the order_service.py

--- shoes-spot/services/order_service.py
+++ shoes-spot/services/order_service.py
@@ -1,28 +1,33 @@
 from services.cart_service import get_cart, clear_cart
 from services.catalog_service import products
 
-orders = []
+class OrderService:
+    def __init__(self):
+        self.orders = []
 
-def place_order(username, address, payment):
-    user_cart = get_cart(username)
-    if not user_cart:
-        return False
+    def place_order(self, username, address, payment):
+        user_cart = get_cart(username)
+        if not user_cart:
+            return False
 
-    for item in user_cart:
-        for p in products:
-            if p["id"] == item["product"]["id"]:
-                if p["stock"] >= item["quantity"]:
-                    p["stock"] -= item["quantity"]
-                else:
-                    return False
+        for item in user_cart:
+            for p in products:
+                if p["id"] == item["product"]["id"]:
+                    if p["stock"] >= item["quantity"]:
+                        p["stock"] -= item["quantity"]
+                    else:
+                        return False
 
-    order = {
-        "user": username,
-        "items": user_cart,
-        "address": address,
-        "payment": payment
-    }
-    orders.append(order)
+        order = {
+            "user": username,
+            "items": user_cart,
+            "address": address,
+            "payment": payment
+        }
+        self.orders.append(order)
 
-    clear_cart(username)
-    return True
+        clear_cart(username)
+        return True
+
+    def get_orders(self):
+        return self.orders
