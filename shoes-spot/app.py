88db7aa0e549140f47ffd729ce94690b748418e1
Viktoria <VGKupenova21@codingburgas.bg>
2025-10-07 21:38:22 +0300

    Add the app.py

--- shoes-spot/app.py
+++ shoes-spot/app.py
@@ -1,17 +1,35 @@
 from flask import Flask, render_template
-from controllers.auth_controller import auth_bp
-from controllers.catalog_controller import catalog_bp
-from controllers.cart_controller import cart_bp
-app = Flask(__name__)
-app.secret_key = "supersecretkey"
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
-app.register_blueprint(auth_bp)
-app.register_blueprint(cart_bp)
-app.register_blueprint(catalog_bp)
+db = SQLAlchemy()
+from services.catalog_service import Product
+from services.order_service import Order, OrderItem
+from services.auth_service import User
+from services.cart_service import CartItem
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    from controllers.auth_controller import auth_bp
+    from controllers.catalog_controller import catalog_bp
+    from controllers.cart_controller import cart_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(cart_bp)
+    app.register_blueprint(catalog_bp)
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
+
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
